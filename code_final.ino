40a2adab72495b9640aae6aff969d203deca3e8f
Moustapha diagne <moustaphadiagne@gmail.com>
2021-07-27 20:27:18 +0000

    commentaires_ajoutes

--- code_final.ino
+++ code_final.ino
@@ -1,24 +1,25 @@
-#include<ESP8266WiFi.h>
+#include<ESP8266WiFi.h>  //importation de bibliotheques
 #include<WiFiClient.h>
-#include <WiFiUDP.h>
-#include<ESP8266WebServer.h>
+#include <WiFiUDP.h>  //pour udp
+#include<ESP8266WebServer.h> //serveur web
 #include <SoftwareSerial.h>
-#include <PZEM004Tv30.h>
-#include <InfluxDb.h>
+
+#include <PZEM004Tv30.h> //pour capteur pzem
+#include <InfluxDb.h>  //transferer influx db via tcp
 
 /* Use software serial for the PZEM
  * Pin 11 Rx (Connects to the Tx pin on the PZEM)
  * Pin 12 Tx (Connects to the Rx pin on the PZEM)
 */
-PZEM004Tv30 pzem(D5, D6);
-const int buttonPin = 4;
-const int marchePin = 5;
+PZEM004Tv30 pzem(D5, D6);  //instanciation objet pzem avec d5 et d6
+const int buttonPin = 4;  // gpio 4 
+const int marchePin = 5; //gpio 5
 int etatdefaut = 0;
 int etatmarche = 0;
-int defaut = 0;
-int running = 0;
-int defaut_now= 0;
-int running_now= 0;
+int defaut = 0;  //variable retour du defaut
+int running = 0;  //variable retour de la position
+int defaut_now= 0;  //derniere valeur avant boucle
+int running_now= 0;  //idem
 String nom_pompe="KMASSAR2";
 String line;
 
@@ -28,11 +29,10 @@ String line;
 //const char* ssid = "FUNBOX_D2F3";
 //const char* password = "KH7dSUAT9fKb4HTjpY";
 
-#define INFLUXDB_HOST "185.98.128.237"   //Enter IP of device running Influx Database
-#define INFLUXDB_PORT 48086
-#define WIFI_SSID "RUT955_TENEZIS"              //Enter SSID of your WIFI Access Point
-#define WIFI_PASS "1234567890"  //Enter Password of your WIFI Access Point
-
+#define INFLUXDB_HOST "185.98.128.237"   //adresse ip raspberry public
+#define INFLUXDB_PORT 48086  //numero de port serveur raspberry public
+#define WIFI_SSID "RUT955_TENEZIS"              //ssid rtu
+#define WIFI_PASS "1234567890"  //mot de passe rtu
 Influxdb influx(INFLUXDB_HOST,INFLUXDB_PORT);
 WiFiServer server(80);
 //byte host[] = {192, 168, 1, 5};
@@ -42,12 +42,12 @@ WiFiServer server(80);
 //WiFiUDP udp;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(buttonPin, INPUT);
-  pinMode(marchePin, INPUT);
+  Serial.begin(9600); //monitaur avec 9600 baud
+  pinMode(buttonPin, INPUT);  //d1 en entree
+  pinMode(marchePin, INPUT);  //d2 en entree
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
-  WiFi.begin(WIFI_SSID,WIFI_PASS );
+  WiFi.begin(WIFI_SSID,WIFI_PASS );  // connection avec wifi
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -56,44 +56,44 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());  //affiche adresse ip esp
   
 
-   long rssi = WiFi.RSSI();
+   long rssi = WiFi.RSSI();  //puissance signal
     Serial.print("signal strength (RSSI):");
     Serial.print(rssi);
     Serial.println(" dBm");
   //server.begin();
 
-  influx.setDb("bnsp");
+  influx.setDb("bnsp");  //base de donnees influx db
 
   Serial.println("Setup Complete.");
 
 }
-int loopCount = 0;
+int loopCount = 0;  //compteur d initialisation
 void loop() {
  
-etatdefaut = digitalRead(buttonPin);
+etatdefaut = digitalRead(buttonPin);  //lecture etat d2 ou gpio 4
 if (etatdefaut == HIGH) {
     // turn LED on
  Serial.println("defaut");
- defaut = 0;
+ defaut = 0;  //variable de retour
   } else {
     // turn LED off
  Serial.println("normal");
-  defaut = 1;
+  defaut = 1;  //variable de retour
   }
 
 
-etatmarche = digitalRead(marchePin);
+etatmarche = digitalRead(marchePin);   //lecture etat d1 ou gpio 5
 if (etatmarche == HIGH) {
     // turn LED on
  Serial.println("marche");
- running = 1;
+ running = 1;  //variable de retour
   } else {
     // turn LED off
  Serial.println("arret");
-  running = 0;
+  running = 0;  //variable de retour
   }
 
   
@@ -147,7 +147,7 @@ if (etatmarche == HIGH) {
   //udp.print(line);
   //udp.endPacket();
 
-
+//on envoie au serveur  soit a chaque 2 minutes ou a chaque changement d etat de la position ou du defaut
 if (loopCount >= 60 || defaut != defaut_now || running != running_now ){
  InfluxData row("pompage");
   row.addTag("nom_pompe", "KMASSAR1");
@@ -163,12 +163,12 @@ if (loopCount >= 60 || defaut != defaut_now || running != running_now ){
   row.addValue("running", running);
  
 
-  influx.write(row);
+  influx.write(row);  //ecriture dans la base
 
   Serial.println(defaut);
   Serial.println(running);
-  defaut_now=defaut;
-  running_now=running;
+  defaut_now=defaut;  //stockage des dernieres valeurs avant la boucle
+  running_now=running; //idem
   loopCount=0;
   }
  // loopCount=0;
